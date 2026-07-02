a6d4ff5fcb97292dcd9a45709b65e752e9b40b65
bktechcomputer2015-pixel <bktech.computer2015@gmail.com>
2026-07-02 18:24:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,25 @@
+const CACHE_NAME = "bktech-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./login.html",
+  "./signup.html",
+  "./profile.html"
+];
+
 self.addEventListener("install", event => {
-  console.log("Service Worker Installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
