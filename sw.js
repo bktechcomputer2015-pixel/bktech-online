9410bedffdf1a9eff18505913311fe4317e1ca68
bktechcomputer2015-pixel <bktech.computer2015@gmail.com>
2026-07-03 10:16:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,13 +5,35 @@ const urlsToCache = [
   "./index.html",
   "./login.html",
   "./signup.html",
-  "./profile.html"
+  "./profile.html",
+  "./courses.html",
+  "./live.html",
+  "./notes.html",
+  "./quiz.html",
+  "./notice.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
